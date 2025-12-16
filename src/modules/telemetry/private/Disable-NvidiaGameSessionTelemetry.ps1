110d24e8d3d88a8375b8cc303173d3e1e07b9f81
agadiffe <10961890+agadiffe@users.noreply.github.com>
2025-12-16 11:52:22 +0100

    Fix Disable-NvidiaGameSessionTelemetry
    The plugin can be in "nvdmsi.inf_amd64_*" or "nvdmi.inf_amd64_*".

--- src/modules/telemetry/private/Disable-NvidiaGameSessionTelemetry.ps1
+++ src/modules/telemetry/private/Disable-NvidiaGameSessionTelemetry.ps1
@@ -16,21 +16,31 @@ function Disable-NvidiaGameSessionTelemetry
     {
         Write-Verbose -Message 'Disabling ''Nvidia Game Session Telemetry Plugin'' ...'
 
-        $DriverStorePath = "$env:SystemDrive\Windows\System32\DriverStore"
-        $NvidiaDisplayPath = "$DriverStorePath\FileRepository\nvdmsi.inf_amd64_*\Display.NvContainer"
-        $NvidiaPluginsSessionPath = "$NvidiaDisplayPath\plugins\Session"
-        $GameSessionTelemetryPluginName = "_NvGSTPlugin.dll"
-        $GSTPluginFilePath = "$NvidiaPluginsSessionPath\$GameSessionTelemetryPluginName"
+        $NvidiaInfPathName = 'nvdmsi', 'nvdmi'
+        foreach ($PathName in $NvidiaInfPathName)
+        {
+            $DriverStorePath = "$env:SystemDrive\Windows\System32\DriverStore"
+            $NvidiaDisplayPath = "$DriverStorePath\FileRepository\$PathName.inf_amd64_*\Display.NvContainer"
+            $NvidiaPluginsSessionPath = "$NvidiaDisplayPath\plugins\Session"
+            $GameSessionTelemetryPluginName = "_NvGSTPlugin.dll"
+            $GSTPluginFilePath = "$NvidiaPluginsSessionPath\$GameSessionTelemetryPluginName"
 
-        if (Test-Path -Path $GSTPluginFilePath)
-        {
-            Set-FileSystemAdminsFullControl -Action 'Grant' -Path $NvidiaPluginsSessionPath
-            Set-FileSystemAdminsFullControl -Action 'Grant' -Path $GSTPluginFilePath
-            Rename-Item -Path $GSTPluginFilePath -NewName "$GameSessionTelemetryPluginName.bak"
-        }
-        else
-        {
-            Write-Verbose -Message '    ''Nvidia Game Session Telemetry Plugin'' file not found'
+            if (Test-Path -Path $GSTPluginFilePath)
+            {
+                Set-FileSystemAdminsFullControl -Action 'Grant' -Path $NvidiaPluginsSessionPath
+                Set-FileSystemAdminsFullControl -Action 'Grant' -Path $GSTPluginFilePath
+                Rename-Item -Path $GSTPluginFilePath -NewName "$GameSessionTelemetryPluginName.bak"
+
+                Write-Verbose -Message "    '$GSTPluginFilePath' disabled"
+            }
+            elseif (Test-Path -Path "$GSTPluginFilePath.bak")
+            {
+                Write-Verbose -Message "    already disabled: '$GSTPluginFilePath'"
+            }
+            else
+            {
+                Write-Verbose -Message "    file not found: '$GSTPluginFilePath'"
+            }
         }
     }
 }
